9539eae6884e8f9c0b27ed94c6e0f18dac122d02
eselenic <selenicaelona@gmail.com>
2020-02-29 12:28:31 -0600

    linking frontend

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 from flask import Flask
+
 from flask import request, redirect
 from flask import render_template
-
 from APIConnectionService import ApiConnectionService
-
 app = Flask(__name__)
-
 apiService = ApiConnectionService()
-
 @app.route('/')
 @app.route('/username/<name>')
 def hello(name: str = None):
-    
-    #withdrawls2 = apiService.GetAllWithdrawals("5e5a90faf1bac107157e0c50").get_withdrawals()
     try:
         return render_template("index.html", username=name)
     except Exception as e:
         return(str(e))
-
 @app.route('/signup', methods = ['POST'])
 def signup():
-    email = request.form['email']
-    print("The email address is '" + email + "'")
+    username = request.form['username']
+    password = request.form['password']
+    print("The username address is '" + username + "'" + password)
     return redirect('/')
-
 if __name__ == '__main__':
     app.run()
