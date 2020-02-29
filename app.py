58794aad685a7c85a7fd51b1e0794c0a07e9a3ac
John Yenter-Briars <jyenterbriars@gmail.com>
2020-02-29 01:03:46 -0600

    Add base example code

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
+
+from flask import render_template
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/username/<name>')
+
+def hello(name: str = None):
+    try:
+        return render_template("index.html", username=name)
+    except Exception as e:
+        return(str(e))
+
+
+
 
 if __name__ == '__main__':
     app.run()
