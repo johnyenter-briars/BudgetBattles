b35ac964f95702af5676fe9e823eb41103addf90
Patrick Fisher <patrick.fisher9999@gmail.com>
2020-02-29 15:04:43 -0600

    Adding Reporting Service

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request, redirect
 from flask import render_template
+from ReportingService import ReportingService
 
 from APIConnectionService import ApiConnectionService
 from database_service import *
+rp = ReportingService()
 
 app = Flask(__name__)
 
@@ -35,6 +37,14 @@ def signup():
     email = request.form['email']
     print("The email address is '" + email + "'")
     return redirect('/')
+
+@app.route('/reportingtest')
+def reporting_test():
+    data = rp.getCurentHistory('5e5a90faf1bac107157e0c50')
+    try:
+        return render_template("index.html", username=name)
+    except Exception as e:
+        return(str(e))
 
 if __name__ == '__main__':
     app.run()
